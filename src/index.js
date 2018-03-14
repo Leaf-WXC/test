51473d1be3421132e8adb0cfa2b658159881b7b4
Leaf-WXC <523045770@qq.com>
2018-03-14 10:35:53 +0800

    修改index.js文件

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+class Greeting extends Component {
+  state={
+    who:'world'
+  }
 
+  switchName=()=>{
+    if (this.state.who==='world')
+      this.setState((prevState,props)=>({who:'react'}))
+    else
+      this.setState((prevState,props)=>({who:'world'}))
+  }
 
+  render() {
+    return (
+      <div>  
+          <h1>{`hello ${this.state.who}`}</h1> 
+          <button onClick={this.switchName}> {this.state.who==='world'?'react':'world'}</button>
+      </div>
+    );
+  }
+} 
+
+      const App=(props,context)=>{
+        return (
+          <div>
+            <Greeting who={'world'} />
+            <Greeting who={'react'} />
+          </div>
+        )
+      }    
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
